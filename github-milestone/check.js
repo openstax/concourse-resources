e7bb0a64d928a124b733e3f2003b89678c9d00ff
tom <tom.g.woodward@gmail.com>
2019-06-26 12:51:24 -0400

    fix filtering github milestones

--- github-milestone/check.js
+++ github-milestone/check.js
@@ -10,7 +10,7 @@ const username = config.source.username;
 const password = config.source.password;
 const repo = config.source.repository;
 
-const endpoint = `http://api.github.com/repos/${repo}/milestones`;
+const endpoint = `https://api.github.com/repos/${repo}/milestones`;
 const headers = {
   'Authorization': 'Basic ' + Buffer.from(username + ':' + password).toString('base64')
 }
@@ -25,26 +25,26 @@ const filterAfter = milestones => {
   }
 };
 
+const filterPrefix = milestones => config.source.prefix
+  ? milestones.filter(milestone => milestone.title.indexOf(config.source.prefix) === 0)
+  : milestones
+
 const writeOut = milestones => process.stdout.write(JSON.stringify(milestones));
 
 const request = fetch(endpoint, {headers})
   .then(response => response.json())
 ;
 
-if (config.source.prefix) {
-  request.then(milestones => milestones
-    .filter(milestone => milestone.title.indexOf(config.source.prefix) === 0)
-  );
-}
-
 if (config.source.mode === 'multiple') {
   request
+    .then(filterPrefix)
     .then(milestones => milestones.reduce((ids, milestone) => ([...ids, milestone.number]), []))
     .then(ids => ([{ids: ids.join(',')}]))
     .then(writeOut)
   ;
 } else {
   request
+    .then(filterPrefix)
     .then(filterAfter)
     .then(milestones => milestones.map(milestone => ({id: milestone.number})))
     .then(ids => ids.reverse()) // concourse expects newer at the top
