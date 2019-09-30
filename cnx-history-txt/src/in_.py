fcfab59a6900078cd912f0ac523d3ff51b4c1b3c
m1yag1 <mike.arbelaez@rice.edu>
2019-09-30 16:31:04 -0500

    Added tests for in.py and other updates
    * Added tests for in_.py and data files.
    * Added pytest-vcr as a test dependency. Updated setup.py
    * Added tests_require to setup.py with pytest and pytest-vcr.
    * Used a vcrpy cassette for the in_.py tests. Other tests will be
    updated later.

--- cnx-history-txt/src/in_.py
+++ cnx-history-txt/src/in_.py
@@ -2,33 +2,37 @@ import json
 import os
 import sys
 
+import requests
 from src.history import get_instance_urls
 
 
 def in_(dest_path, in_stream):
     input = json.load(in_stream)
 
-    version = input["version"]["date"]
+    date = input["version"]["date"]
     instance = input["source"]["instance"]
+    webview_url = get_instance_urls(instance)["webview_url"]
 
-    version_path = os.path.join(dest_path, "version")
-    urls_path = os.path.join(dest_path, "urls.json")
+    response = requests.get(f"{webview_url}/version.txt")
+    app_versions = response.json()
 
-    with open(version_path, "w") as file:
-        file.write(version)
+    with open(os.path.join(dest_path, "version"), "w") as file:
+        file.write(date)
 
-    with open(urls_path, "w") as file:
+    with open(os.path.join(dest_path, "urls.json"), "w") as file:
         json.dump(get_instance_urls(instance), file)
 
-    return version
+    with open(os.path.join(dest_path, "app_versions.json"), "w") as file:
+        json.dump(app_versions, file)
+
+    return {"version": {"date": date}}
 
 
 def main():
     dest_path = sys.argv[1]
     print(f"Output dir {dest_path}", file=sys.stderr)
     version = in_(dest_path, sys.stdin)
-    print(f"Version is {version}", file=sys.stderr)
-    print(json.dumps({"version": {"date": version}}))
+    print(json.dumps(version))
 
 
 if __name__ == '__main__':
