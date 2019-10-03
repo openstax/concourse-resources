4961b06976c1086c19cd38982eedcbe8ac115143
m1yag1 <mike.arbelaez@rice.edu>
2019-10-03 11:26:58 -0500

    fix incorrect legacy url

--- cnx-history-txt/src/history.py
+++ cnx-history-txt/src/history.py
@@ -8,16 +8,16 @@ HISTORY_URL_TEMPLATE = "{0}/history.txt"
 RELEASE_DELIMITER = "==============================="
 VERSIONS_DELIMITER = "-------------------------------"
 INSTANCE_URLS = {
-    "prod": {"legacy_url": "https://legacy.cnx-org",
+    "prod": {"legacy_url": "https://legacy.cnx.org",
              "archive_url": "https://archive.cnx.org",
              "webview_url": "https://cnx.org"},
-    "dev": {"legacy_url": "https://legacy-dev.cnx-org",
+    "dev": {"legacy_url": "https://legacy-dev.cnx.org",
             "archive_url": "https://archive-dev.cnx.org",
             "webview_url": "https://dev.cnx.org"},
-    "qa": {"legacy_url": "https://legacy-qa.cnx-org",
+    "qa": {"legacy_url": "https://legacy-qa.cnx.org",
            "archive_url": "https://archive-qa.cnx.org",
            "webview_url": "https://qa.cnx.org"},
-    "staging": {"legacy_url": "https://legacy-staging.cnx-org",
+    "staging": {"legacy_url": "https://legacy-staging.cnx.org",
                 "archive_url": "https://archive-staging.cnx.org",
                 "webview_url": "https://staging.cnx.org"},
 }
