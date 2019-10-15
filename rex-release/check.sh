3f820a14c4bc2ad9820ab93d7e04b365cca8a59c
tom <tom.g.woodward@gmail.com>
2019-10-15 15:14:02 -0400

    fix argument error in rex-release

--- rex-release/check.sh
+++ rex-release/check.sh
@@ -4,7 +4,7 @@ set -e -u
 exec 3>&1 # make stdout available as fd 3 for the result
 exec 1>&2 # redirect all output to stderr for logging
 
-payload=$(mktemp /tmp/resource-in.xxxxxx)
+payload=$(mktemp /tmp/resource-in.XXXXXX)
 
 cat > "$payload" <&0
 
